f2792f6d27a758f123bf0b09aa91196c85c762d1
Ryan <ryan81.park@gmail.com>
2016-06-20 10:51:15 +1000

    Display clubs' header

--- Components/selectTeams.jsx
+++ Components/selectTeams.jsx
@@ -47,7 +47,7 @@ var SelectTeams = React.createClass({
 			<div>
 			<div className="dropdown">
 			 <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    			Clubs
+    			{this.state.data.shortName || 'Clubs'}
     				<span className="caret"></span>
   				</button>
 				<ul className="dropdown-menu" aria-labelledby="dropdownMenu1">{clubs}</ul>

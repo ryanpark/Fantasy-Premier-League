c4a9a1730bef5ee05326412455112bbaf6949eab
Ryan <ryan81.park@gmail.com>
2016-06-28 23:34:29 +1000

    Render lists

--- Components/team.jsx
+++ Components/team.jsx
@@ -1,71 +1,66 @@
 import React from 'react';
 var d3 = require('d3-format');
 
-
+var resultLists =  {
+       defenders: [],
+       midfielders: [],
+       forwards : [],
+       keepers: []
+};
 var Team = React.createClass({
-   render : function () {
-      
-       if (typeof this.props.selectPlayers === 'undefined') {
-           return null;
-       } else {
-       var pLists =_.chain(this.props.selectPlayers.players)
-            .groupBy(function(item) {
-            var positionType = '';
-            switch (item.position) {
+   getInitialState : function () {
+     return {
+        a: []
+     }
+   },
+   renderLists : function() {
+       var p = this.props.selectPlayers.players.map(function(p,m) {
+           switch (p.position) {
                 case 'Centre Back':
                 case 'Left-Back':
                 case 'Right-Back':
-                positionType = 'defenders';
+                resultLists.defenders.push(<li>{p.name}</li>);
                 break;
                 case 'Defensive Midfield':
                 case 'Central Midfield':
                 case 'Attacking Midfield':
                 case 'Right Midfield':
                 case 'Left Midfield':
-                positionType = 'midfielders';
+                resultLists.midfielders.push(<li>{p.name}</li>);
                 break;
                 case 'Left Wing':
                 case 'Right Wing':
                 case 'Centre Forward':
-                positionType = 'forwards';
+                resultLists.forwards.push(<li>{p.name}</li>);
                 break;
                 case 'Keeper':
-                positionType = 'keepers';
+                resultLists.keepers.push(<li>{p.name}</li>);
                 break;
-            }
-            return positionType;
-        }).map(function(item, n) {
-            console.log(item['defenders'])
-        }).value();
-           /*
-         var playerLists = _.chain(this.props.selectPlayers.players).filter(function(o) { 
-            if (o.position == "Keeper") {
-                return position.keepers.push(o);   
-            }  
-            if (o.position == 'Left-Back' || o.position == 'Right-Back' || o.position == "Centre Back") {
-                return position.defenders.push(o);
-            }
-            if (o.position == 'Central Midfield' || o.position == 'Attacking Midfield' || o.position == "Defensive Midfield") {
-                return position.midfields.push(o);
-            }
-            if (o.position == 'Centre Forward' || o.position == 'Right Wing' || o.position == "Left Wing") {
-                return position.forwards.push(o);
-            }
-        }).map(function(p) {
-            console.log(p)
-        });*/
-        
-        console.log(pLists)
-         /*
-         playerLists = this.props.selectPlayers.players.map(function(p) {
-              return (<li>{p.name}</li>)
-         }.bind(this));*/
-         
-         return (
+           }
+          
+       }.bind(this));
+       
+       return p;
+       
+   },
+   render : function () {
+      
+       if (typeof this.props.selectPlayers === 'undefined') {
+           return null;
+       } else {
+        this.renderLists();
+        return (
            <div>
             <h3>{this.props.data.name}</h3>
                 <ul className="list-unstyled">
+                    <h4>Keepers</h4>
+                    {resultLists.keepers}
+                    <h4>Defenders</h4>
+                    {resultLists.defenders}
+                    <h4>Midfielders</h4>
+                    {resultLists.midfielders}
                     <h4>Forwards</h4>
+                    {resultLists.forwards}
                 </ul>
            </div>
           );     

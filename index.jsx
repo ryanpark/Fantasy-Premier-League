ee825f7a62fa772ba31c754bb9974464c1f816f6
Ryan <ryan81.park@gmail.com>
2016-07-31 23:29:36 +1000

    Added Redux

--- index.jsx
+++ index.jsx
@@ -4,6 +4,8 @@ require("./public/style/style.css")
 require("./node_modules/font-awesome/css/font-awesome.css");
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import $ from 'jquery';
 require("./node_modules/bootstrap/dist/js/bootstrap.min.js");
 

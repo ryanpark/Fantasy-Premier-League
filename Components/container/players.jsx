64c4728473c74f04b2422b9385c6fce5362e7afd
Ryan <ryan81.park@gmail.com>
2016-09-15 16:27:03 +1000

    Popover to display players' information

--- Components/container/players.jsx
+++ Components/container/players.jsx
@@ -1,52 +1,59 @@
 import React from 'react';
 import Popover from 'react-bootstrap/lib/Popover';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
-import Button from 'react-bootstrap/lib/Button';
 import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
 import Overlay from 'react-bootstrap/lib/Overlay';
 
 
+const pop = function(content) {
+    return (
+        <Popover id="popover-positioned-right" title={content.name}>
+            <ul className="list-group list-unstyled">
+                <li>Market Value : {content.marketValue}</li>
+                <li>Position : {content.position}</li>
+                <li>Nationality : {content.nationality}</li>
+                <li>Contract : {content.contractUntil}</li>
+                <li>Number : {content.jerseyNumber}</li>
+            </ul>
+        </Popover> 
+   )
+}
+        
 class Players extends React.Component {
  removePlayer(d, e) {
      this.props.bindRemovePlayer(d);
  }   
  render() {
-        const keepers =  this.props.list.Keeper.map(e => typeof e.name =='undefined' ? '' : <div><img src={e.logo} width="10%" height="10%" />
-        <ButtonToolbar>
-        <Button onClick={this.handleClick}>
-          Holy guacamole!
-        </Button>
-
-        <Overlay
-          show={true}
-          placement="bottom"
-          container={this}
-          containerPadding={20}
-        >
-          <Popover id="popover-contained" title="Popover bottom">
-            <strong>Holy guacamole!</strong> Check this info.
-          </Popover>
-        </Overlay>
-      </ButtonToolbar>
-         <a href="#" className="">{e.name}</a> <i className="fa fa-minus-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)
-        const defenders =  this.props.list.Defenders.map(e => typeof e.name =='undefined' ? '' : <div><img src={e.logo} width="10%" height="10%" /> <a href="#" className="">{e.name}</a> <i className="fa fa-minus-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)
-        const midfields =  this.props.list.Midfield.map(e => typeof e.name =='undefined' ? '' : <div><img src={e.logo} width="10%" height="10%" /> <a href="#" className="">{e.name}</a> <i className="fa fa-minus-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)
-        const forwards =  this.props.list.Forwards.map(e => typeof e.name =='undefined' ? '' : <div><img src={e.logo} width="10%" height="10%" /> <a href="#" className="">{e.name}</a> <i className="fa fa-minus-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)     
+        const keepers =  this.props.list.Keeper.map((e, i)  => typeof e.name =='undefined' ? '' : <div className={i}><img src={e.logo}  />
+      
+            <OverlayTrigger trigger="focus" placement="top" overlay={pop(e)}>
+                <a href="#" className="">{e.name}</a>
+            </OverlayTrigger>
+       <i className="fa fa-times-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)
+        const defenders =  this.props.list.Defenders.map((e, i) => typeof e.name =='undefined' ? '' : <div className={i}><img src={e.logo}  /> 
+        
+            <OverlayTrigger trigger="focus" placement="top" overlay={pop(e)}>
+                <a href="#" className="">{e.name}</a>
+            </OverlayTrigger>
+        <i className="fa fa-times-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)
+        const midfields =  this.props.list.Midfield.map((e, i) => typeof e.name =='undefined' ? '' : <div className={i}><img src={e.logo}  /> 
+         <OverlayTrigger trigger="focus" placement="top" overlay={pop(e)}>
+                <a href="#" className="">{e.name}</a>
+            </OverlayTrigger>
+        <i className="fa fa-times-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)
+        const forwards =  this.props.list.Forwards.map((e, i) => typeof e.name =='undefined' ? '' : <div className={i}><img src={e.logo}  />
+        <OverlayTrigger trigger="focus" placement="top" overlay={pop(e)}>
+                <a href="#" className="">{e.name}</a>
+        </OverlayTrigger>
+        <i className="fa fa-times-circle removeIcon" onClick={this.removePlayer.bind(this, e)}></i></div>)     
   
     
      return (
-            <div>
-             {this.props.list.Keeper.length > 1 ? <h3>Keepers</h3> : ''  }
-             <div>{keepers}</div>
-             {this.props.list.Defenders.length > 1 ? <h3>Defenders</h3> : ''  }
-             <div>{defenders}</div>
-             {this.props.list.Midfield.length > 1 ? <h3>midfields</h3> : ''  }
-             <div>{midfields}</div>
-             {this.props.list.Forwards.length > 1 ? <h3>Forwards</h3> : ''  }
-             <div>{forwards}</div>
-             
-           
-            
+             <div>
+                <div className="Keepers">{keepers}</div>
+                <div className="Defenders">{defenders}</div>
+                <div className="Midfields">{midfields}</div>
+                <div className="Forwards">{forwards}</div>
              </div>
      )
  
